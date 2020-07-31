14708a62c2cc66cfca35ddbcd91bc3eb963304af
TheSirC <clocloranely@hotmail.fr>
2020-07-31 12:57:58 +0200

    Version de base
    Les problèmes suivants existent dans cette révision :
    - les pages "lenses" et "LEDs" causent une panique pour le programme
    avec le message suivant :
    
        `'The call to the database for $equipment did not go well:
        DeserializationError(Custom { kind: UnexpectedEof,
        error: "failed to fill whole buffer" })'`
    
    - les filtres ne sont pas reliés à un changement de lignes pour la table

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $(".sampleSlider").ionRangeSlider({
+        type: "double",
+        grid: true,
+        skin: "sharp"
+    });
+    $("#stock").DataTable();
+} );
