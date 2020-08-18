0fe74480bbbb044f119da4a241d88955bb39fc5e
TheSirC <clocloranely@hotmail.fr>
2020-08-18 16:21:21 +0200

    Ajout de la logique pour le filtrage
    Cette logique est censée fonctionner pour les filtres multiples et les simples

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,18 @@
+var filter = (currentValue) => {
+    return function(settings, data, index, rowData, counter) {
+            var min = currentValue.slider.data("from");
+            var max = currentValue.slider.data("to");
+
+            var data_to_filter = data[currentValue.column_number].split(" - ", 2);
+            var data_to_filter_min = parseFloat(data_to_filter[0]) || min;
+            var data_to_filter_max = parseFloat(data_to_filter[1]) || max;
+            return ((isNaN(min) && isNaN(max)) ||
+                (isNaN(min) && data_to_filter_max <= max) ||
+                (min <= data_to_filter_min && isNaN(max)) ||
+                (min <= data_to_filter_min && data_to_filter_max <= max))
+        }
+};
+
 $(document).ready(function() {
     $(".sampleSlider").ionRangeSlider({
         type: "double",
@@ -5,4 +20,4 @@ $(document).ready(function() {
         skin: "sharp"
     });
     $("#stock").DataTable();
-} );
+});
